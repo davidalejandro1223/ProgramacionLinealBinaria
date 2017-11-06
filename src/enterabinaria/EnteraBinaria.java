8873a221d379f035648346b1dd5c6a4be38536c8
David Aparicio <davidalejandro1223@hotmail.com>
2017-11-05 19:42:01 -0500

    Proyeto terminado y funcional

--- src/enterabinaria/EnteraBinaria.java
+++ src/enterabinaria/EnteraBinaria.java
@@ -4,15 +4,81 @@ package enterabinaria;
 //import scpsolver.constraints.LinearSmallerThanEqualsConstraint;
 //import scpsolver.lpsolver.LinearProgramSolver;
 //import scpsolver.lpsolver.SolverFactory;
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+import javax.swing.JTable;
 import scpsolver.problems.LPSolution;
 import scpsolver.problems.LPWizard;
+import scpsolver.problems.LPWizardConstraint;
 //import scpsolver.problems.LinearProgram;
 
 public class EnteraBinaria {
 
-    /**
-     * @param args the command line arguments
-     */
+    private LPWizard lpw;
+    private boolean minProblem;
+    private int numVar;
+    private int numRes;
+    boolean confIniComplete = false;
+
+    public EnteraBinaria() {
+        this.lpw = new LPWizard();
+    }
+
+    public void setMinProblem(boolean minProblem) {
+        this.minProblem = minProblem;
+    }
+
+    public void setNumVar(int numVar) {
+        this.numVar = numVar;
+    }
+
+    public void setNumRes(int numRes) {
+        this.numRes = numRes;
+    }
+
+    public String Solucionar(JTable tabla) {
+        lpw.setMinProblem(minProblem);
+        for (int i = 0; i <= numRes; i++) {
+            LPWizardConstraint lpwc = null;
+            for (int j = 1; j <= numVar; j++) {
+                if (i == 0 && j <= numVar) {
+                    this.lpw.plus("x" + j, Integer.parseInt(tabla.getValueAt(i, j).toString()));
+                } else if(i!=0){
+                    int val;
+                    if (!this.confIniComplete) {
+                        val = Integer.parseInt(tabla.getValueAt(i, numVar + 2).toString());
+                        String op = tabla.getValueAt(i, numVar + 1).toString();
+                        if(op.compareTo("<=")==0){
+                            op = ">=";
+                        }else if(op.compareTo(">=")==0){
+                            op = "<=";
+                        }else if(op.compareTo(">")==0){
+                            op = "<";
+                        }else{
+                            op = ">";
+                        }
+                        lpwc = this.lpw.addConstraint("c"+i, val, op);
+                        this.confIniComplete = true;
+                        j = j-1;
+                    } else {
+                        try{
+                            val = Integer.parseInt(tabla.getValueAt(i, j).toString());
+                            lpwc.plus("x"+j, val);
+                        }catch(NumberFormatException nfe){
+                            System.out.println(nfe.getMessage());
+                        }
+                    }
+                }
+            }
+            if(lpwc != null){
+                lpwc.setAllVariablesBoolean();
+                this.confIniComplete = false;
+            }           
+        }
+        
+        LPSolution sol = lpw.solve();
+        return sol.toString();
+    }
+
     public static void main(String[] args) {
         LPWizard lpw = new LPWizard();
         lpw.plus("x1", 20).plus("x2", 40).plus("x3", 20).plus("x4", 15).plus("x5", 30);
@@ -21,9 +87,8 @@ public class EnteraBinaria {
         lpw.addConstraint("c3", 25, ">=").plus("x1", 8).plus("x2", 10).plus("x3", 2).plus("x4", 1).plus("x5", 10).setAllVariablesBoolean();
         lpw.setMinProblem(false);
         LPSolution sol = lpw.solve();
-        //LPSolution sol = lpw.solve(SolverFactory.newDefault());
-        System.out.println(sol);
-        
+        System.out.println(sol.toString());
+
     }
 
 }
